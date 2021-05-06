b42869b32d177b32274b902997984f0f546439c3
Juliana Freitas <julianachristello@gmail.com>
2021-05-05 22:59:54 -0300

    iniciando componentização

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Pagina from "./Pagina";
+import Confirmacao from "./Confirmacao";
+
+export default function App() {
+    return (
+      <div>
+        <Pagina />
+        <Confirmacao />
+      </div>
+    )
+  }
---
+++ src/components/Pagina.js
@@ -0,0 +1,7 @@
+export default function Pagina() {
+    return (
+      <div>
+        llll
+      </div>
+    )
+  }
